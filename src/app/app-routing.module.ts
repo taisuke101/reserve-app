4dec3b66f86d4dba8ebdb5ca3ee2f9dc0010c7b5
Taisuke Yamamoto <uwattotaitai@gmail.com>
2020-04-26 23:04:22 +0900

    フロントエンド部分作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: 'product', pathMatch: 'full'},
+  { path: '', redirectTo: 'product', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    ProductModule
+    ProductModule,
+    AuthModule
   ],
   exports: [RouterModule]
 })
